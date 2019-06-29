b40a79d95d024f98c0052ca0db7e073dee1244d3
ChungkwonRyu <ryuch91@kaist.ac.kr>
2019-06-29 18:16:35 +0900

    Get result for metric2(Scan Fairness)

--- src/run.py
+++ src/run.py
@@ -4,6 +4,7 @@ from matplotlib import animation
 from numpy import array, mgrid, zeros, linspace
 from numpy.random import choice
 import csv
+from math import sqrt
 
 from src.models import agent
 import src.conf as cfg
@@ -27,8 +28,13 @@ class Simulation:
         self.nlistLCs = [] # leftCells 개수를 리스트로 저장
         self.nlistlistLCs = [] #leftCells 개수 리스트의 리스트
 
+        self.listFN = [] # Fairness 저장을 위한 리스트
+        self.scanCounter = {} # 각 셀의 scan 횟수를 저장하는 dict
+
     # nUAV x 반복횟수 만큼 시뮬 진행. 각 시뮬은 step 수 만큼 진행
     def run(self):
+
+        self.init_scan_counter()
 
         # 3개의 UAV 개수에 대해 실험 진행
         for i in range(len(self.nUAVs)):
@@ -56,6 +62,7 @@ class Simulation:
                     self.nlistLCs.append(len(self.leftCells))
                     swarm.sense_target()
                     self.nlistST.append(swarm.count_sensed_target())
+                    self.update_scan_counter(swarm)
 
                 self.map.reboot_phe()
                 swarm.init_agents(nUAVs)
@@ -63,12 +70,14 @@ class Simulation:
                 # Metric 측정된 값들을 하나의 리스트에 기록
                 self.nlistlistST.append(self.nlistST)
                 self.nlistlistLCs.append(self.nlistLCs)
+                self.listFN.append(self.cal_fairness())
                 self.init_metric() # 메트릭 관련 변수들(새 repeat 마다 초기화되어야하는것들) 초기화
 
                 print(str(i) + '-' + str(j) + '-rand' + ' finished!')  # 진행사항을 알기위한 콘솔 출력
 
             self.write_csv_metric0(self.nlistlistST, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Rand')
             self.write_csv_metric1(self.nlistlistLCs, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Rand')
+            self.write_csv_metric2(self.listFN, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Rand')
             self.init_upper_metric() # 전체 step x repeat 끝날때마다 초기화되어야 하는 것들 초기화
 
             for j in range(self.nRepeat):
@@ -86,18 +95,21 @@ class Simulation:
                     self.nlistLCs.append(len(self.leftCells))
                     swarm.sense_target()
                     self.nlistST.append(swarm.count_sensed_target())
+                    self.update_scan_counter(swarm)
 
                 self.map.reboot_phe()
                 swarm.init_agents(nUAVs)
 
                 self.nlistlistST.append(self.nlistST)
                 self.nlistlistLCs.append(self.nlistLCs)
+                self.listFN.append(self.cal_fairness())
                 self.init_metric()
 
                 print(str(i) + '-' + str(j) + '-phe' + ' finished!')  # 진행사항을 알기위한 콘솔 출력
 
             self.write_csv_metric0(self.nlistlistST, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Phe')
             self.write_csv_metric1(self.nlistlistLCs, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Phe')
+            self.write_csv_metric2(self.listFN, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Phe')
             self.init_upper_metric()
 
             for j in range(self.nRepeat):
@@ -115,18 +127,21 @@ class Simulation:
                     self.nlistLCs.append(len(self.leftCells))
                     swarm.sense_target()
                     self.nlistST.append(swarm.count_sensed_target())
+                    self.update_scan_counter(swarm)
 
                 self.map.reboot_phe()
                 swarm.init_agents(nUAVs)
 
                 self.nlistlistST.append(self.nlistST)
                 self.nlistlistLCs.append(self.nlistLCs)
+                self.listFN.append(self.cal_fairness())
                 self.init_metric()
 
                 print(str(i)+'-'+str(j)+'-flex'+' finished!') # 진행사항을 알기위한 콘솔 출력
 
             self.write_csv_metric0(self.nlistlistST, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Flex')
             self.write_csv_metric1(self.nlistlistLCs, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Flex')
+            self.write_csv_metric2(self.listFN, self.map.mapSize, self.map.scene, self.map.nT, nUAVs, 'Flex')
             self.init_upper_metric()
 
     def draw(self,name, i_n,j_repeat,k_step):
@@ -150,11 +165,21 @@ class Simulation:
 
         plt.close('all')
 
+    # 각 Repeat 마다 사전에 초기화해야 되는 것들
     def pre_init_metric(self):
         LCs = []
         for cell in self.map.Cells:
             LCs.append(cell)
         self.leftCells = LCs
+        
+        # scanCounter value들을 모두 0으로 설정
+        for key, val in self.scanCounter.items():
+            self.scanCounter[key] = 0
+
+    # 시뮬 생성시 스캔카운터 초기화(여기서 map.cell 통해 key를 넣어주고 이후로는 값만 초기화)
+    def init_scan_counter(self):
+        for key in self.map.Cells:
+            self.scanCounter[key] = 0
 
     def init_metric(self):
         self.nlistST = []
@@ -167,6 +192,8 @@ class Simulation:
         self.nlistLCs = []
         self.nlistlistLCs = []
 
+        self.listFN = []
+
     def cal_left_cells(self,swarm):
         for agent in swarm.agents:
             if agent.position in self.leftCells:
@@ -174,6 +201,36 @@ class Simulation:
             for block in agent.movable_blocks:
                 if block in self.leftCells:
                     self.leftCells.remove(block)
+
+    # Swarm을 통해 scanCounter를 업데이트
+    def update_scan_counter(self, swarm):
+        for agent in swarm.agents:
+            self.scanCounter[agent.position] += 1
+            for block in agent.movable_blocks:
+                self.scanCounter[block] += 1
+
+    # scanCounter로부터 fairness 를 계산해서 return
+    def cal_fairness(self):
+        FN = 0.0
+        sum = 0
+        sqSum = 0
+        vals = []
+        for key, val in self.scanCounter.items():
+            sum += val
+            vals.append(val)
+        size = len(self.scanCounter)
+        if size != 0:
+            mean = sum/size
+        else:
+            mean = 0.0
+            return 0.0
+
+        for val in vals:
+            sqSum += (mean - val)**2
+
+        FN = sqrt((float(sqSum)/size))
+
+        return FN
 
     # Met0: 타겟 수 측정을 위한 csv 파일에 작성 (data는 list of list)
     def write_csv_metric0(self, data, mapSize, scene, nT, nU, method):
@@ -201,6 +258,17 @@ class Simulation:
 
         f.close()
 
+    # Met2: Scan Fairness 측정을 위한 csv 파일에 작성 (data 는 list)
+    def write_csv_metric2(self, data, mapSize, scene, nT, nU, method):
+        filename='Map'+str(mapSize[0])+'_Scene' + str(scene) + '_T' +str(nT)+'_U'+str(nU)+'_' + method + '_Met2.csv'
+        f = open(filename, 'w',newline='')
+        wr = csv.writer(f)
+
+        for fn in data:
+            tmp = [fn]
+            wr.writerow(tmp)
+
+        f.close()
 
 if __name__ == '__main__':
     # Generate Map
